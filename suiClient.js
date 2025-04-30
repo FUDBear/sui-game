724b456148a93bcb0e11a441a9ecd120331d3c1e
FUDBear <jmackb85@gmail.com>
2025-04-29 20:43:30 -0700

    Fix signer imports for SUI SDK compatibility

--- suiClient.js
+++ suiClient.js
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
-import { Ed25519Keypair } from '@mysten/sui.js/keypairs/ed25519';
-import { RawSigner } from '@mysten/sui.js/signer';
-import { Connection, JsonRpcProvider } from '@mysten/sui.js/providers';
-
 dotenv.config();
+
+import { Ed25519Keypair } from '@mysten/sui.js/keypairs/ed25519.js';
+import { RawSigner } from '@mysten/sui.js/dist/cjs/cryptography/raw-signer.js';
+import { JsonRpcProvider, Connection } from '@mysten/sui.js/dist/cjs/providers/json-rpc-provider.js';
 
 const keypair = Ed25519Keypair.deriveKeypair(process.env.SUI_MNEMONIC);
 const connection = new Connection({
